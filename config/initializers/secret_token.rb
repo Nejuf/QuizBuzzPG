8a6fe5a4847d195b8549c70306ec6ce65b337cf6
Nejuf <nejuf1@gmail.com>
2014-03-08 22:00:18 -0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+QuizBuzz::Application.config.secret_token = '4e400ad48e8b86d2ba98b89cf01f1050b55bcaa9c83bbb87828737efe925972413e02d82675db917b74662715e3336ede1cf54ee06917f61ee08aa9432ae36af'
