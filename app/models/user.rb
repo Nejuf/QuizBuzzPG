b82071341b9bfb94729f029df9bdd044af06ceb1
Nejuf <nejuf1@gmail.com>
2014-03-23 16:13:23 -0700

    Adds user password

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,10 @@
+require 'bcrypt'
+
 class User < ActiveRecord::Base
-  attr_accessible :email, :name
+  attr_accessible :email, :name, :password
 
   validates :email, presence: true
+  validates :password_digest, presence: { message: "Password can't be blank."}
 
   before_save :ensure_defaults
 
@@ -9,6 +12,15 @@ class User < ActiveRecord::Base
     self.is_mod.nil? ? false : self.is_mod
   end
 
+  def password=(pass)
+    self.password_digest = BCrypt::Password.create(pass)
+  end
+
+  def is_password?(pass)
+    BCrypt::Password.new(self.password_digest).is_password?(pass)
+  end
+  
+end
   private
   def ensure_defaults
     self.is_mod ||= false
